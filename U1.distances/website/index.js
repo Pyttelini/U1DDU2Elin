80abb2dc0b77e745befd8d05071d282598e69b5d
Pyttelini <elin.f.lindberg@gmail.com>
2024-11-19 18:41:25 +0100

    Tabellen inlagd

--- U1.distances/website/index.js
+++ U1.distances/website/index.js
@@ -7,4 +7,62 @@ for (let city of cities ){
     pcreate.classList.add("cityBox");
 
 }
+let TableDiv = document.querySelector("#table");
+let pcreateFirstGrid = document.createElement("p");
+
+
+TableDiv.appendChild(pcreateFirstGrid);
+
+for(let city of cities){
+    let pcreate = document.createElement("p");
+    TableDiv.appendChild(pcreate);
+    pcreate.textContent = city.id;
+    pcreate.classList.add("cell");
+    pcreate.classList.add("head_row");
+
+}
+for (let i = 0; i < cities.length; i++) {
+    let city = cities[i];
+    let pcreate = document.createElement("p");
+    TableDiv.appendChild(pcreate);
+    pcreate.textContent = city.id + "-" + city.name;
+    pcreate.classList.add("cell");
+    pcreate.classList.add("head_column");
+
+    if (i % 2 === 0) {
+        pcreate.classList.add("even_row");
+    }
+
+    for (let j = 0; j < cities.length; j++) {
+        if (i === j) {
+            let pcreate = document.createElement("p");
+            TableDiv.appendChild(pcreate);
+            pcreate.classList.add("cell");
+            if (i % 2 === 0) {
+                pcreate.classList.add("even_row");
+            }
+            if (j % 2 === 0) {
+                pcreate.classList.add("even_col");
+            }
+        } else {
+            let distance = distances.find(d => 
+                (d.city1 === city.id && d.city2 === cities[j].id) || 
+                (d.city2 === city.id && d.city1 === cities[j].id)
+            );
+            let pcreate = document.createElement("p");
+            TableDiv.appendChild(pcreate);
+            pcreate.textContent = distance ? distance.distance / 10 : "";
+            pcreate.classList.add("cell");
+            if (i % 2 === 0) {
+                pcreate.classList.add("even_row");
+            }
+            if (j % 2 === 0) {
+                pcreate.classList.add("even_col");
+            }
+        }
+    }
+}
+
+
+
 
